c31fcf5c312d3dccab080341abc22014e8a8d8dd
Samuel <samuel.pntrn@eduge.ch>
2022-01-27 16:09:53 +0100

    V-0.2.0
    Changed messages display

--- index.php
+++ index.php
@@ -50,161 +50,41 @@
   <div id="FCContainer" class="columns">
     <aside class="column">
       <div class="column">
-      <div class="card">
-        <div class="card-image">
-          <figure class="image"><img src="https://source.unsplash.com/h-ACUrBngrw/1280x720" alt="Image"></figure>
-        </div>
-        <div class="card-content">
-          <div class="media">
-            <div class="media-left">
-              <figure class="image is-48x48"><img src="https://avatars.dicebear.com/api/initials/john%20doe.svg" alt="Image"></figure>
+        <div class="card">
+          <div class="card-image">
+            <figure class="image"><img src="https://source.unsplash.com/h-ACUrBngrw/1280x720" alt="Image"></figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-48x48"><img src="https://avatars.dicebear.com/api/initials/john%20doe.svg" alt="Image"></figure>
+              </div>
+              <div class="media-content">
+                <p class="title is-4 no-padding">Lead Developer</p>
+              </div>
             </div>
-            <div class="media-content">
-              <p class="title is-4 no-padding">Lead Developer</p>
+            <div class="content">
+              The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.
+              <div class="background-icon"><span class="icon-twitter"></span></div>
             </div>
           </div>
-          <div class="content">
-            The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.
-            <div class="background-icon"><span class="icon-twitter"></span></div>
-          </div>
-        </div>
         </div>
       </div>
       <div class="column">
         <div class="compose has-text-centered"><a class="button is-primary is-block is-bold"><span class="compose">Postez quelque chose!</span></a></div>
       </div>
     </aside>
-    <div id="message-feed" class="column is-10 messages hero is-fullheight">
-
-      <div id="inbox-messages" class="inbox-messages">
-        <div id="msg-card-0" data-preview-id="0" class="card active">
-          <div class="card-content">
-            <div class="msg-header"><span class="msg-from"><small>From: Drake Ratke II</small></span> <span class="msg-timestamp"></span> <span class="msg-attachment"><i class="fa fa-paperclip"></i></span></div>
-            <div class="msg-subject"><span class="msg-subject"><strong id="fake-subject-1">Modi consequatur est rerum et autem vita</strong></span></div>
-            <div class="msg-snippet">
-              <p id="fake-snippet-1">Ut eum deserunt velit eos quibusdam sunt odit quas aut.</p>
-            </div>
-          </div>
+    <div id="post-feed" class="column is-10 hero is-fullheight">
+      <article class="message">
+        <div class="message-header">
+          <p>Hello World</p>
+          <button class="delete" aria-label="delete"></button>
         </div>
-        <div id="msg-card-1" data-preview-id="1" class="card">
-          <div class="card-content">
-            <div class="msg-header"><span class="msg-from"><small>From: Selena Lindgren</small></span> <span class="msg-timestamp"></span> <span class="msg-attachment"><i class="fa fa-paperclip"></i></span></div>
-            <div class="msg-subject"><span class="msg-subject"><strong id="fake-subject-1">Consequatur et ea in.</strong></span></div>
-            <div class="msg-snippet">
-              <p id="fake-snippet-1">Sint eligendi iusto voluptas.
-                Et perspiciatis eveniet qui esse illo nam.
-                Sunt eum deserunt dolorem voluptates optio perferendis et enim.
-                Facere commodi ut.
-                In molestias asperiores animi.</p>
-            </div>
-          </div>
+        <div class="message-body">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
         </div>
-        <div id="msg-card-2" data-preview-id="2" class="card">
-          <div class="card-content">
-            <div class="msg-header"><span class="msg-from"><small>From: Tyler McKenzie</small></span> <span class="msg-timestamp"></span> <span class="msg-attachment"><i class="fa fa-paperclip"></i></span></div>
-            <div class="msg-subject"><span class="msg-subject"><strong id="fake-subject-1">Natus sed maiores totam minus alias quod</strong></span></div>
-            <div class="msg-snippet">
-              <p id="fake-snippet-1">Dolorum perferendis aliquid.
-                A quia et et rerum est excepturi.
-                Incidunt numquam repellendus quia eum ut harum expedita in dolores.
-                Corporis suscipit nihil.</p>
-            </div>
-          </div>
-        </div>
-        <div id="msg-card-3" data-preview-id="3" class="card">
-          <div class="card-content">
-            <div class="msg-header"><span class="msg-from"><small>From: Colten DuBuque</small></span> <span class="msg-timestamp"></span> <span class="msg-attachment"><i class="fa fa-paperclip"></i></span></div>
-            <div class="msg-subject"><span class="msg-subject"><strong id="fake-subject-1">Error voluptas harum rerum quia velit ad</strong></span></div>
-            <div class="msg-snippet">
-              <p id="fake-snippet-1">Non non odit.
-                Eveniet at et voluptatibus odio sit eum.
-                Natus mollitia et porro minus.</p>
-            </div>
-          </div>
-        </div>
-        <div id="msg-card-4" data-preview-id="4" class="card">
-          <div class="card-content">
-            <div class="msg-header"><span class="msg-from"><small>From: Deshawn Ziemann</small></span> <span class="msg-timestamp"></span> <span class="msg-attachment"><i class="fa fa-paperclip"></i></span></div>
-            <div class="msg-subject"><span class="msg-subject"><strong id="fake-subject-1">Temporibus ut repellat voluptates est do</strong></span></div>
-            <div class="msg-snippet">
-              <p id="fake-snippet-1">Ut esse eos aut.
-                Voluptatibus libero praesentium.
-                Odio quaerat omnis tenetur consequatur velit dolorem dolorum quas quos.
-                Doloremque animi fugiat quis atque.</p>
-            </div>
-          </div>
-        </div>
-        <div id="msg-card-5" data-preview-id="5" class="card">
-          <div class="card-content">
-            <div class="msg-header"><span class="msg-from"><small>From: Mrs. Brannon Funk</small></span> <span class="msg-timestamp"></span> <span class="msg-attachment"><i class="fa fa-paperclip"></i></span></div>
-            <div class="msg-subject"><span class="msg-subject"><strong id="fake-subject-1">Dolor aut nemo impedit facilis natus et </strong></span></div>
-            <div class="msg-snippet">
-              <p id="fake-snippet-1">Itaque ut modi dolorem voluptates eius rerum et doloremque voluptatem.
-                Voluptas necessitatibus laboriosam.
-                Delectus corporis voluptatem quae.
-                Aut dolores facere est doloribus.
-                Ut quia nisi earum ut.</p>
-            </div>
-          </div>
-        </div>
-        <div id="msg-card-6" data-preview-id="6" class="card">
-          <div class="card-content">
-            <div class="msg-header"><span class="msg-from"><small>From: Rita Wintheiser</small></span> <span class="msg-timestamp"></span> <span class="msg-attachment"><i class="fa fa-paperclip"></i></span></div>
-            <div class="msg-subject"><span class="msg-subject"><strong id="fake-subject-1">Aliquam dolorum expedita ut qui autem.</strong></span></div>
-            <div class="msg-snippet">
-              <p id="fake-snippet-1">Quos voluptates vero tempore reprehenderit eum quas.
-                Recusandae sapiente et rem eveniet non.
-                Facilis consectetur asperiores magnam odit earum est corporis nisi.
-                Sunt quaerat beatae accusantium est voluptas voluptas voluptas.</p>
-            </div>
-          </div>
-        </div>
-        <div id="msg-card-7" data-preview-id="7" class="card">
-          <div class="card-content">
-            <div class="msg-header"><span class="msg-from"><small>From: Ms. Tyrese Jaskolski</small></span> <span class="msg-timestamp"></span> <span class="msg-attachment"><i class="fa fa-paperclip"></i></span></div>
-            <div class="msg-subject"><span class="msg-subject"><strong id="fake-subject-1">Maiores sapiente sed voluptatem sequi do</strong></span></div>
-            <div class="msg-snippet">
-              <p id="fake-snippet-1">Quisquam illo enim maxime debitis nobis nobis repudiandae.
-                In voluptatem ipsum ut ut enim.
-                Eligendi nesciunt pariatur quis ut vero.
-                Sapiente odio aut.</p>
-            </div>
-          </div>
-        </div>
-        <div id="msg-card-8" data-preview-id="8" class="card">
-          <div class="card-content">
-            <div class="msg-header"><span class="msg-from"><small>From: Domenica Larkin</small></span> <span class="msg-timestamp"></span> <span class="msg-attachment"><i class="fa fa-paperclip"></i></span></div>
-            <div class="msg-subject"><span class="msg-subject"><strong id="fake-subject-1">Assumenda est earum quaerat temporibus q</strong></span></div>
-            <div class="msg-snippet">
-              <p id="fake-snippet-1">Et aliquid possimus aut.
-                Facilis omnis sed voluptatum amet enim.
-                Illum quae est excepturi omnis.
-                Cum tempore eius dolores omnis labore sed.</p>
-            </div>
-          </div>
-        </div>
-        <div id="msg-card-9" data-preview-id="9" class="card">
-          <div class="card-content">
-            <div class="msg-header"><span class="msg-from"><small>From: Floy Bartell Jr.</small></span> <span class="msg-timestamp"></span> <span class="msg-attachment"><i class="fa fa-paperclip"></i></span></div>
-            <div class="msg-subject"><span class="msg-subject"><strong id="fake-subject-1">Ut id et nam.</strong></span></div>
-            <div class="msg-snippet">
-              <p id="fake-snippet-1">Cum non sint pariatur facilis ea.
-                Voluptas et quia dolor animi.
-                Repellat quos qui et consequuntur voluptas a.</p>
-            </div>
-          </div>
-        </div>
-        <div id="msg-card-10" data-preview-id="10" class="card">
-          <div class="card-content">
-            <div class="msg-header"><span class="msg-from"><small>From: Rosalyn Runolfsson</small></span> <span class="msg-timestamp"></span> <span class="msg-attachment"><i class="fa fa-paperclip"></i></span></div>
-            <div class="msg-subject"><span class="msg-subject"><strong id="fake-subject-1">Et sit et inventore minima eum ratione.</strong></span></div>
-            <div class="msg-snippet">
-              <p id="fake-snippet-1">Rerum consequuntur voluptatem sit architecto.
-                Aut earum repellendus ipsa sit ut minima.</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      </article>
+    
     </div>
   </div>
 </body>
